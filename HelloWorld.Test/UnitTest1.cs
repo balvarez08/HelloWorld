dae679d2e4eee93fb22b418302a1072f20f924fb
balvar05 <30730765+balvar05@users.noreply.github.com>
2017-08-04 11:29:30 -0500

    Add files via upload

---
+++ HelloWorld.Test/UnitTest1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using HelloWorld.Data;
+using System.Collections.Generic;
+using System.Linq;
+using HelloWorld.Web.Repository;
+
+namespace HelloWorld.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private HelloWorldModel _db;
+        [TestMethod]
+        public void Check_If_Data_Exist_Table()
+        {
+            var testRequest = SetupTestRecord();
+            var testId = testRequest.id;
+            var HWRepo = new HelloWorldRepository();
+
+            HWRepo.Add(testRequest);
+
+            var hwdata = _db.DataInputs
+               .Where(rr => rr.id == testRequest.id)
+               .SingleOrDefault();
+            Assert.IsNotNull(hwdata);
+
+            CleanUpTestRecord(testId);
+        }
+        
+        private DataInputs SetupTestRecord()
+        {
+            _db = new HelloWorldModel();
+            
+            var newData = new DataInputs
+            {
+               id = 1,
+               DataInputText = "Hello World"
+            };
+
+            _db.DataInputs.Add(newData);
+            _db.SaveChanges();
+
+            return newData;
+        }
+
+        private void CleanUpTestRecord(int hwId)
+        {
+            var removeData = _db.DataInputs
+                .Where(rr => rr.id == hwId)
+                .SingleOrDefault();
+
+            if (removeData != null)
+            {
+                _db.DataInputs.Remove(removeData);
+                _db.SaveChanges();
+            }
+        }
+    }
+}
